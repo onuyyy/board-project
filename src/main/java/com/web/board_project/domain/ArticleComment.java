5da56891115c5c43107da334089c67f5934875ab
onuyyy <qazxsw9026@naver.com>
2025-06-11 11:24:35 +0900

    #13 - AuditingFields 추출
    생성자, 생성일시, 수정자, 수정일시 공통 필드로빼서 Article, ArticleComments에 상속 시킴

--- src/main/java/com/web/board_project/domain/ArticleComment.java
+++ src/main/java/com/web/board_project/domain/ArticleComment.java
@@ -2,12 +2,7 @@ package com.web.board_project.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 
-import java.time.LocalDateTime;
 import java.util.Objects;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -19,7 +14,7 @@ import java.util.Objects;
         @Index(columnList = "createdBy")
 })
 @Entity
-public class ArticleComment {
+public class ArticleComment extends AuditingFields {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,11 +23,6 @@ public class ArticleComment {
     @Setter @ManyToOne(optional = false) private Article article; // 연관 관계 스타일
     // @Setter private long articleId; 관계형 데이터베이스 스타일
     @Setter @Column(nullable = false, length = 500) private String content;
-
-    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;
-    @CreatedBy @Column(nullable = false, length = 100) private String createdBy;
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;
-    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;
 
     private ArticleComment(Article article, String content) {
         this.article = article;
